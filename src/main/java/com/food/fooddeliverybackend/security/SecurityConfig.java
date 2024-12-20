c3af848c9734e7488f1c682866c4b5e7e16fc5ca
= <maheshkadambala18@gmail.com>
2024-12-20 17:53:32 +0530

    fix swagger config and disable security

--- src/main/java/com/food/fooddeliverybackend/security/SecurityConfig.java
+++ src/main/java/com/food/fooddeliverybackend/security/SecurityConfig.java
@@ -1,4 +1,20 @@
 package com.food.fooddeliverybackend.security;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
 public class SecurityConfig {
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .authorizeHttpRequests(authorize -> authorize
+                        .anyRequest().permitAll() // Allow all requests without authentication
+                )
+                .csrf().disable()
+                .headers().frameOptions().disable();
+        return http.build();
+    }
 }
